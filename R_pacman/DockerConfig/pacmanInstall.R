450c040381abbe379349720ed205624136901eb7
Tania Allard <taniar.allard@gmail.com>
2018-08-16 12:13:19 +0100

    Update gitignore

--- R_pacman/DockerConfig/pacmanInstall.R
+++ R_pacman/DockerConfig/pacmanInstall.R
@@ -1,5 +1,6 @@
 # Install packages using pacman
 pacman::p_install_version(
-  c( "data.table", "plyr"),
+  c( "data.table", "plyr", "ggplot2", "httr", "httr", "jsonlite", "lubridate"),
   c( "1.10.4.-2", "1.8.4")
   )
+
